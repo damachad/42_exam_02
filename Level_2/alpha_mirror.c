f244f1747078fa310a5a718bb854ca6a6975d2b3
David Correia <davidmachado98@hotmail.com>
2023-06-06 11:37:36 +0100

    Add files via upload

---
+++ Level_2/alpha_mirror.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   alpha_mirror.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: damachad <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/06 10:45:52 by damachad          #+#    #+#             */
+/*   Updated: 2023/06/06 10:46:02 by damachad         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	repeat_alpha(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (str[i] >= 65 && str[i] <= 90)
+		{
+			str[i] = 90 - str[i] + 65;
+		}
+		if (str[i] >= 97 && str[i] <= 122)
+		{
+			str[i] = 122 - str[i] + 97;
+		}
+		write(1, &str[i], 1);
+		i++;
+	}
+}
+
+int	main(int argc, char **argv)
+{
+	if (argc == 2)
+		repeat_alpha(argv[1]);
+	write(1, "\n", 1);
+	return (0);
+}
