cba8d0cfaa144d6271086108e7b683c7de959855
David Correia <davidmachado98@hotmail.com>
2023-06-06 15:47:31 +0100

    Add files via upload

---
+++ Level_2/camel_to_snake.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   camel_to_snake.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: damachad <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/06 15:34:16 by damachad          #+#    #+#             */
+/*   Updated: 2023/06/06 15:34:17 by damachad         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	camel_to_snake(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (str[i] >= 65 && str[i] <= 90)
+		{
+			write(1, "_", 1);
+			str[i] += 32;
+		}	
+		write(1, &str[i], 1);
+		i++;
+	}
+}
+
+int	main(int argc, char **argv)
+{
+	if (argc == 2)
+		camel_to_snake(argv[1]);
+	write(1, "\n", 1);
+	return (0);
+}
