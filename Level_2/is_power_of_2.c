22045c493352f3002fc74fbd663f62e2e62ed630
David Correia <davidmachado98@hotmail.com>
2023-06-13 12:25:55 +0100

    Add files via upload

---
+++ Level_2/is_power_of_2.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   is_power_of_2.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: damachad <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/13 12:13:11 by damachad          #+#    #+#             */
+/*   Updated: 2023/06/13 12:13:12 by damachad         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int	is_power_of_2(unsigned int n)
+{
+	while (n > 1)
+	{
+		if (n % 2 != 0)
+			return (0);
+		n /= 2;
+	}
+	return (1);
+}
+/*
+int	main(int argc, char **argv)
+{
+	if (argc == 2)
+	{
+		if (atoi(argv[1]) >= 0)
+			printf("%d\n", is_power_of_2(atoi(argv[1])));
+		else
+			printf("Please insert a positive number\n");
+	}
+	else
+		printf("Please insert a positive number\n");
+	return (0);
+}
+*\
